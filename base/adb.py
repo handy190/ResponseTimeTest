2ddc8c856c11df0e03eebbac6f9a7b2129f4b304
hongzhi <handy190@163.com>
2018-05-21 01:32:08 +0800

    add base function to ADB class

--- base/adb.py
+++ base/adb.py
@@ -2,10 +2,12 @@
 import os
 import string
 import subprocess
-
+import datetime
 
 
 class ADB(object):
+
+
     """
     开启一个cmd窗口并且执行command_text命令，将stdout输出到指定文件中
     """
@@ -27,12 +29,18 @@ class ADB(object):
         devices = result.partition('\n')[2].replace('\n', '').split('\tdevice')
         return [device for device in devices if len(device) > 2]
 
+
+    """push 文件到手机"""
     def push(self, from_computor, to_phone):
         return self.cmd("push " + from_computor + " " + to_phone)
 
+
+    """pull 手机文件"""
     def pull(self, from_phone, to_computor):
         return self.cmd("pull " + from_phone + " " + to_computor)
 
+
+    """覆盖安装apk"""
     def install(self, apk_file):
         return self.cmd("install -r " + apk_file)
 
@@ -45,6 +53,16 @@ class ADB(object):
     def start(self, activity):
         return self.cmd("shell am start " + activity)
 
+    def screenShot(self):
+
+        #   获取系统当前时间
+        now = datetime.datetime.now()  # 时间数组格式
+
+        #   转换为指定格式时间戳
+        timestamp = now.strftime("%Y%m%d%H%M%S")
+
+        self.cmd("shell screencap -p /mnt/sdcard/ScreenShot/screen_shot" + timestamp + ".png")
+
 
 test = ADB()
-test.kill("com.tencent.news")
+test.screenShot()
