b06a6c5f79b4cf386bc37021768e9229c4ac7bbe
hongzhi <handy190@163.com>
2018-05-23 17:02:08 +0800

    fix bug

---
+++ base/Activities.py
@@ -0,0 +1,40 @@
+from enum import Enum, unique
+
+@unique
+class Acivities(Enum):
+
+    MESSAGE = "",
+
+    CONTACTS = "",
+
+    CAMERA = "",
+
+    GALLERY = "",
+
+    SETTINGS = "",
+
+    MUSIC = "",
+
+    CALCULATOR = "",
+
+    SYSTEM_CLOCK = "",
+
+    FILE_MANAGER = "",
+
+    BROWSER = "",
+
+    PHONE = "",
+
+    QQ = "",
+
+    WECHAT = "",
+
+    TENCENT_VIDEO = "com.tencent.news/com.tencent.news.activity.SplashActivity"
+
+    JD_SHOPPING = ""
+
+    DOUYIN = "",
+
+    TAOBAO = "",
+
+    TENCENT_NEWS = ""
--- base/adb.py
+++ base/adb.py
@@ -1,57 +1,93 @@
-# encoding=utf-8
+# -*- coding: utf-8 -*-
+
+
+
 import os
+import platform
 import string
 import subprocess
 import datetime
 
 
+
 class ADB(object):
 
+    def __init__(self, serial=None):
+        self._serial = serial
+
+    """获取手机序列号"""
+    def serial(self):
+        if self._serial:
+            return self._serial
+        if os.name == 'nt':
+            self._serial = self.execute("getprop | findstr ro.serialno")
+        else:
+            self._serial = self.execute("getprop | grep ro.serialno")
+        return self._serial
 
     """
-    开启一个cmd窗口并且执行command_text命令，将stdout输出到指定文件中
+    开启一个execute窗口并且执行command_text命令，将stdout输出到指定文件中
     """
-    def cmd(self, command):
+    def execute(self, command):
         command_result = ''
         command_text = 'adb %s' % command
-        write_text = open(r"/Users/hongzhi/Test/cmd.txt", "w")
-        results = subprocess.Popen(command_text, shell=True, stdout=write_text,).stdout
+        write_text = open(r"/Users/hongzhi/Test/raw_data.txt", "a+") # 每个应用对应一个文件比较合适
+        pipe = subprocess.Popen(command_text, shell=True, stdout=write_text).stdout
         write_text.close()
-        while 1:
-            line = results
+        while True:
+            line = pipe
             if not line: break
             command_result += line
         return command_result
 
-
+    """
+    获取手机列表
+    """
     def devices(self):
-        result = self.cmd("devices")
+        result = self.execute("devices")
         devices = result.partition('\n')[2].replace('\n', '').split('\tdevice')
         return [device for device in devices if len(device) > 2]
 
 
     """push 文件到手机"""
     def push(self, from_computor, to_phone):
-        return self.cmd("push " + from_computor + " " + to_phone)
+        return self.execute("push " + from_computor + " " + to_phone)
 
 
     """pull 手机文件"""
     def pull(self, from_phone, to_computor):
-        return self.cmd("pull " + from_phone + " " + to_computor)
+        return self.execute("pull " + from_phone + " " + to_computor)
 
 
-    """覆盖安装apk"""
-    def install(self, apk_file):
-        return self.cmd("install -r " + apk_file)
+    """覆盖安装apk,并且默认允许了所有权限"""
+    def install(self, apk_path):
+        return self.execute("install -r -g " + apk_path)
 
+    """卸载apk"""
     def uninstall(self, package):
-        return self.cmd("shell pm uninstall " + package)
+        return self.execute("shell pm uninstall " + package)
 
+    """进入adb shell环境"""
     def shell(self, command):
-        return self.cmd("shell " + command)
+        return self.execute("shell " + command)
 
+    """启动一个activity界面"""
     def start(self, activity):
-        return self.cmd("shell am start " + activity)
+        return self.execute("shell am start " + activity)
+
+    """获取logcat"""
+    def getLogcat(self, command):
+        return self.execute("logcat " + command)
+
+    """获取指定字段的logcat"""
+    def getLogcatString(self, str):
+        if os.name == 'nt': # Windows 系统
+            return self.getLogcat("| findstr " + str)
+        else:
+            return self.getLogcat("| grep " + str)
+
+    def clearLogcat(self):
+        return self.getLogcat("-c")
 
     def screenShot(self):
 
@@ -61,8 +97,10 @@ class ADB(object):
         #   转换为指定格式时间戳
         timestamp = now.strftime("%Y%m%d%H%M%S")
 
-        self.cmd("shell screencap -p /mnt/sdcard/ScreenShot/screen_shot" + timestamp + ".png")
+        return self.execute("shell screencap -p /mnt/sdcard/ScreenShot/screen_shot" + timestamp + ".png")
 
 
 test = ADB()
-test.screenShot()
+print("正在获取字段")
+test.getLogcatString("Displayed")
+print("获取字段结束")
