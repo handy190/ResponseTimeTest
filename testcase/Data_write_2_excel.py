31cc0ae3e5cef4cd5a4b0e2dffa71763f1e68561
hongzhi <hongzhi@konka.com>
2018-06-06 19:59:28 +0800

    fix function

--- testcase/Data_write_2_excel.py
+++ testcase/Data_write_2_excel.py
@@ -1,47 +1,48 @@
 # -*- coding: utf-8 -*-
 
 import xlsxwriter
-import re
 
-
-class w2e(object):
-
+"""
+数据处理类，将结果写入excel表格中生成测试报告
+"""
+class Data(object):
 
     def filter_cold_data(self):
 
-        # with open("../TestData/cold_raw_data.txt", 'r') as file1:
-        #     print(re.findall(r'', file1.read()))
-        f1 = open("../TestData/cold_raw_data.txt", 'r')
-        while True:
-            line = f1.readline()
-            if not line:
-                break
-            s1 = re.findall(r'TotalTime: (\w+)*', line, re.M)  #多行匹配
+        with open("../TestData/cold_raw_data.txt", 'r') as file1:
+            lines = file1.readlines()
+            for i in lines:
+                if 'TotalTime' in i:
+                    data = str(i.split(': ')[1])  # 注意split(': ')后面有个空格
 
-            print(s1)
 
     def filter_hot_data(self):
         with open("../TestData/hot_raw_data.txt", 'r') as file2:
-            print(file2.read())
+            lines = file2.readlines()
+            for i in lines:
+                if 'TotalTime' in i:
+                    data = str(i.split(': ')[1])
 
-    def write_data(self):
+    def write_data_2_excel(self):
         # Create an new Excel file and add a worksheet.
         workbook = xlsxwriter.Workbook('../report/TestReport.xlsx')
         worksheet = workbook.add_worksheet()
+
 
         # Widen the first column to make the text clearer.
         worksheet.set_column('A:A', 20)
 
         # Add a bold format to use to highlight cells.
-        bold = workbook.add_format({'bold': True, 'font_color': 'red'})
+        cell_format = workbook.add_format({'bold': True, 'font_color': 'red'})
 
         # Write some simple text.
         worksheet.write('A1', 'Hello')
 
         # Text with formatting.
-        worksheet.write('A2', 'World', bold)
+        worksheet.write('A2', 'World', cell_format)
 
         # Write some numbers, with row/column notation.
+        cell_format.set_font_color('green')
         worksheet.write(2, 0, 123)
         worksheet.write(3, 0, 123.456)
 
@@ -53,4 +54,4 @@ class w2e(object):
 
 
 if __name__ == '__main__':
-    w2e().filter_cold_data()
+    Data().filter_cold_data()
